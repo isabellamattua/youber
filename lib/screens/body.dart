5fa5f89701f05de47cb5063218b1feb6e4254d9a
isabella.mattua <isa_mattua@hotmail.com>
2020-10-17 15:42:41 -0400

    Basic Widgets

--- lib/screens/body.dart
+++ lib/screens/body.dart
@@ -1,9 +1,8 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:youber/constants.dart';
 import 'package:youber/screens/TextWithUnderlineDeco.dart';
+import 'package:youber/screens/OrderBox.dart';
 
 
 import 'header_bar.dart';
@@ -17,30 +16,12 @@ class Body extends StatelessWidget {
             children: <Widget>[
               header_bar(size: size),
               TextWithUnderlineDeco( text: "Orders"),
-              Center(
-                child: Container(
-                  margin: EdgeInsets.symmetric(
-                      horizontal: kDefaultPadding,
-                      vertical: kDefaultPadding
-                  ),
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  height: 100,
-                  decoration: BoxDecoration(
-                      color: kPrimaryColor.withOpacity(0.7),
-
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          offset:  Offset(0,10),
-                          blurRadius: 50,
-                          color: kPrimaryColor.withOpacity(0.23),
-                        )
-                      ]
-                  ),
-
-
-                ),
-              )
+              OrderBox(text: "Order:1546215\nGerge"),
+              OrderBox(text: "Order:1546215\nGerge"),
+              OrderBox(text: "Order:1546215\nGerge"),
+              OrderBox(text: "Order:1546215\nGerge"),
+              OrderBox(text: "Order:1546215\nGerge"),
+              OrderBox(text: "Order:1546215\nGerge")
 
             ]
         )
